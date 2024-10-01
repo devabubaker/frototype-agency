c1c0115dcf96f8e398a97b29d24de99590bc3373
Taha Baig <connectmtb@gmail.com>
2024-10-01 14:34:19 +0500

    Project Updated

---
+++ assets/js/constant.js
@@ -0,0 +1,49 @@
+export const projectData = [
+  {
+    id: 0,
+    linkUrl: "#",
+    imgUrl: "/assets/images/project-img-1.webp",
+    title: "Restaurant dashboard",
+    subTitle: "Logo and branding",
+  },
+
+  {
+    id: 1,
+    linkUrl: "#",
+    imgUrl: "/assets/images/project-img-2.webp",
+    title: "Restaurant dashboard",
+    subTitle: "Logo and branding",
+  },
+
+  {
+    id: 2,
+    linkUrl: "#",
+    imgUrl: "/assets/images/project-img-3.webp",
+    title: "Restaurant dashboard",
+    subTitle: "Logo and branding",
+  },
+
+  {
+    id: 3,
+    linkUrl: "#",
+    imgUrl: "/assets/images/project-img-2.webp",
+    title: "Restaurant dashboard",
+    subTitle: "Logo and branding",
+  },
+
+  {
+    id: 4,
+    linkUrl: "#",
+    imgUrl: "/assets/images/project-img-3.webp",
+    title: "Restaurant dashboard",
+    subTitle: "Logo and branding",
+  },
+
+  {
+    id: 5,
+    linkUrl: "#",
+    imgUrl: "/assets/images/project-img-1.webp",
+    title: "Restaurant dashboard",
+    subTitle: "Logo and branding",
+  },
+];
---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+// Project Sec
+
+import { projectData } from "./constant.js";
+const projectContainer = document.querySelector(
+  ".project-sec .project-container"
+);
+
+const projectsData = () => {
+  projectContainer.innerHTML = projectData
+    .map(
+      (item) => `
+    <div class="project-frame">
+              <a href="${item.linkUrl}">
+                <div class="image">
+                  <img
+                    src="${item.imgUrl}" 
+                    alt="Project Image"
+                  />
+                </div>
+
+                <div class="content">
+                  <h3>${item.title}</h3>
+
+                  <hr />
+
+                  <h5>${item.subTitle}</h5>
+                </div>
+              </a>
+            </div>
+    `
+    )
+    .join(""); // join the mapped array into a string
+};
+
+projectsData();
+
+// Services Sec Tab
+
+const servicesTab = () => {
+  const cusTabs = document.querySelectorAll(
+    ".services-sec .services-container .services-frame .cus-tabs button"
+  );
+
+  cusTabs.forEach((tabs) => {
+    tabs.addEventListener("click", () => {
+      cusTabs.forEach((tabs2) => {
+        tabs2.classList.remove("active");
+      });
+      tabs.classList.add("active");
+    });
+  });
+};
+
+servicesTab();
+
+// Sticky Nav
+
+const stickyNav = () => {
+  const header = document.querySelector(".header");
+
+  window.addEventListener("scroll", () => {
+    header.classList.toggle("active", pageYOffset > 100);
+  });
+};
+
+stickyNav();
